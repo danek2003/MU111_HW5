8814d0620e717c902bef155b10653cf2dc6ef514
Danilenko Evgeny <Danek@MacBook-Pro-Danilenko.local>
2014-10-21 02:42:53 +1100

    создал неудаляемые и не вставляемые 10 ячеек

---
+++ LectureViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LectureViewController.swift
+//  MU111-hw3
+//
+//  Created by Danilenko Evgeny on 16.10.14.
+//  Copyright (c) 2014 Danilenko Evgeny. All rights reserved.
+//
+
+import Cocoa
+
+class LectureViewController: NSObject {
+
+}

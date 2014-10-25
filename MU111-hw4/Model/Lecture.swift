22437f2d5b273da943abf4b24167d1944227bf30
Danilenko Evgeny <Danek@MacBook-Pro-Danilenko.local>
2014-10-26 00:35:15 +1100

    Синглтон, работать с URLSession закачиваем файлик который содержит json , парами этот файл и отдаем на табличку , работа с табличками

---
+++ MU111-hw4/Model/Lecture.swift
@@ -0,0 +1,13 @@
+//
+//  Lecture.swift
+//  MU111-hw4
+//
+//  Created by Danilenko Evgeny on 26.10.14.
+//  Copyright (c) 2014 Danilenko Evgeny. All rights reserved.
+//
+
+import UIKit
+
+class Lecture: NSObject {
+   var name = ""
+}
